03793eae16dc89d79b922318336bce2a0a30d920
seoyooung <sylim0318@naver.com>
2022-10-14 16:45:32 +0900

    [ADD] post request

---
+++ src/main/java/com/example/flux/payload/request/PostRequest.java
@@ -0,0 +1,13 @@
+package com.example.flux.payload.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PostRequest {
+
+    private String title;
+    private String content;
+
+}
