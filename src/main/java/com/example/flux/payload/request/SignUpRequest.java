ea65fe184aee0238f56174d1687e7eff74ae5688
seoyooung <sylim0318@naver.com>
2022-10-18 08:46:01 +0900

    [ADD] sign up request

---
+++ src/main/java/com/example/flux/payload/request/SignUpRequest.java
@@ -0,0 +1,13 @@
+package com.example.flux.payload.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SignUpRequest {
+
+    private String id;
+    private String password;
+
+}
