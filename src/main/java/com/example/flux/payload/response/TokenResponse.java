c7db90376d75dac47d9372cb87f5330222ace4d0
seoyooung <sylim0318@naver.com>
2022-10-18 08:45:36 +0900

    [ADD] token response

---
+++ src/main/java/com/example/flux/payload/response/TokenResponse.java
@@ -0,0 +1,14 @@
+package com.example.flux.payload.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class TokenResponse {
+
+    private String accessToken;
+
+}
